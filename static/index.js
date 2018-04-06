15312d177ecd58711146ddab29c1487d6655eae7
Fredriclam <fredriclam@live.com>
2018-04-06 16:07:22 -0400

    Loading screen

--- static/index.js
+++ static/index.js
@@ -1,7 +1,8 @@
 // Stage initial size
 var defaultSize = [192, 108];
 var aspectRatio = defaultSize[0] / defaultSize[1];
-var bgImagePath = "static/images/yul.jpg";
+var bgImagePathLarge = "static/images/yul.jpg";
+var bgImagePathMobile = "static/images/yul_small.jpg"; // Smaller texture
 
 // Set up stage and renderer
 const app = new PIXI.Application({
@@ -17,26 +18,56 @@ document.body.appendChild(renderer.view);
 // List sprites out
 const sprites = {};
 
-// Set state to
-var state = loading;
-
-// Attach resize callback to onresize event listener
-window.onresize = function(event) {
-  // Resize app
-  resizePixi();
-  // Resize background
-  stretchToFit();
-};
-resizePixi();
-// Loading
-PIXI.loader.add(bgImagePath).load((loader, resources) => {
-  state = doneLoading;
-  console.log("Resources loaded.");
-})
-
+// Set state to initial state
+var state = init;
 // Global declarations
 var bgTexture;
 var bgSprite;
+
+// Call initial state once
+init();
+
+// Initial state
+function init() {
+  // Resize app
+  resizePixi();
+
+  // Create loading screen
+  loadingText = new PIXI.Text("Loading...",
+    {fontFamily :"Arial",
+    fontSize: 36,
+    fill: "white",
+    align:"center"});
+  loadingText.x = window.innerWidth/2;
+  loadingText.y = window.innerHeight/2;
+  loadingText.anchor.x = 0.5;
+  loadingText.anchor.y = 0.5;
+  app.stage.addChild(loadingText);
+  // Render once
+  renderer.render(stage);
+
+  // Attach resize callback to onresize event listener
+  window.onresize = function(event) {
+    // Resize app
+    resizePixi();
+    // Resize background
+    stretchToFit();
+  };
+
+  bgImagePath = bgImagePathLarge;
+  // Check for small screens
+  if (window.innerWidth < 800){
+    bgImagePath = bgImagePathMobile;
+  }
+  // Load background image
+  PIXI.loader.add(bgImagePath).load((loader, resources) => {
+    state = doneLoading;
+    console.log("Resources loaded.");
+    console.log("Loaded " + bgImagePath);
+  })
+
+  state = loading;
+}
 
 // Loading state
 function loading() {
@@ -70,15 +101,14 @@ function stretchToFit(){
 
 // Main (animation) state
 function mainState() {
-  renderer.render(stage);
-  // window.requestAnimationFrame(() => {renderer.render(stage)});
+  window.requestAnimationFrame(() => {renderer.render(stage)});
 };
 
 // Resize callback
 function resizePixi() {
   // Resize Pixi app
   let w = window.innerWidth+1; // Magic pixel...
-  let h = window.innerHeight;
+  let h = window.innerHeight+1;
   renderer.resize(w,h);
 }
 
